3188173973ab27afd0c3b27bc3dea5c5e9ce36e1
Jimmy Beckett <jimmybeckett1@gmail.com>
2025-10-29 01:03:36 -0700

    Add helix

---
+++ unroll.sh
@@ -0,0 +1,4 @@
+rm -rf ~/.config/helix/config.toml ~/.config/helix/languages.toml ~/.config/helix/themes
+ln -s ~/dotfiles/helix/config.toml ~/.config/helix/config.toml
+ln -s ~/dotfiles/helix/languages.toml ~/.config/helix/languages.toml
+ln -s ~/dotfiles/helix/themes ~/.config/helix/themes
